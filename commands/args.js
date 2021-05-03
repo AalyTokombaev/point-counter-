73fe8b5b03b4f61150abbc9108b4af115901351e
OlderNed <alexiver96@hotmail.com>
2021-05-03 15:32:56 +0200

    nothing changed

--- commands/args.js
+++ commands/args.js
@@ -3,8 +3,9 @@ module.exports = {
     "description" : "testing the args command",
     execute(message, args) {
         message.channel.send("the first argumet is: " +  args[0]);
-        username = args[0].slice(3, -1);
+        username = args[0].slice(3, -1); // this be a misnomer
         result = message.client.users.fetch(username);
+        console.log(result);
         result.then(res => {
             message.channel.send(res.avatarURL());
         })
